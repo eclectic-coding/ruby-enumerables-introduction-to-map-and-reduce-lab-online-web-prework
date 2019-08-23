afc01b6880654beb8811d168cf90bd5bd545ec13
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-08-23 13:42:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,12 +35,5 @@ end
 
 # Reducing 
 def reduce_to_any_true(source_array) 
-  i = 0
-  while i < source_array.length do 
-    if (source_array[i] == true )
-      return true
-    end
-    i += 1
-  end
-  return false
+  source_array.any?{|n|}
 end
